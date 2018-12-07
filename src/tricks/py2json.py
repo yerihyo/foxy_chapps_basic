9ded54a1d9139782d951d90d713a807ed78402c0
Moonyoung Kang <yerihyo@gmail.com>
2018-12-07 00:02:04 -0800

    (#2) py2json working

--- src/tricks/py2json.py
+++ src/tricks/py2json.py
@@ -1,22 +1,10 @@
-from datetime import datetime
-from functools import reduce
+import ast
+import json
 
-import pytz
 from flask import make_response
 
-import settings
-from utils.utils import dt2timezone, env2v_or_none,lmap
-import os
-from collections import defaultdict
-from datetime import datetime
-from urllib.request import urlretrieve
-from urllib.parse import urljoin
-from zipfile import ZipFile
-
-
-def get(pystr):
-    # raise Exception( type(j_env) )
-
-    return make_response("hello world")
-
+def put(s, indent,):
+    j = ast.literal_eval(s)
+    s_JSON = json.dumps(j, indent=indent, ensure_ascii=False,)
+    return make_response(s_JSON)
 
